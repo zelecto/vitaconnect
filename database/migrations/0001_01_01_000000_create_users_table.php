fca2e1e740194dc156c0c65afaa3bc509a2959e6
zelecto <ca346364@gmail.com>
2024-09-12 11:54:34 -0500

    modificacion de mi modelo usuario, migracion para la base de datos

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,12 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('last_name');
+            $table->string('email')->primary();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('gender');
+            //$table->rememberToken();
             $table->timestamps();
         });
 
