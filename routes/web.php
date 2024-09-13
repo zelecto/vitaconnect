871cb2210b77ca81be2bc04119927b042e5e729a
zelecto <ca346364@gmail.com>
2024-09-13 10:57:33 -0500

    Implementacion de nuevas rutas para el control de los metodos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class);
+Route::get('/', LoginController::class);
+Route::post('/Autenticar', [LoginController::class, 'autenticar']);
+
+Route::post('/RegisterUser', [UserController::class, 'store']);
+
+Route::get('/Home/{user}', HomeController::class);
