27afd8c5f6234d3fbd74b6c8c6acafb731d0a840
zelecto <ca346364@gmail.com>
2024-09-13 15:09:48 -0500

    validacion de credenciales

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -12,12 +14,25 @@ class LoginController extends Controller
         return view('login.LoginView');
     }
 
-    public function autenticar(Request $recuest)
+    public function autenticar(Request $request)
     {
-        $user = User::where('email', $recuest->email)->first();
-        if ($user && $user->password === $recuest->password) {
-            return redirect('/Home/' . $recuest->email);
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+
+        $user = User::where('email', $request->email)->first();
+
+
+        if ($user && Hash::check($request->password, $user->password)) {
+
+            session(['user_id' => $user->id]);
+
+            return redirect('/Home/' . urlencode($user->email));
         }
+
         return redirect('/')->with('error', 'Credenciales incorrectas.');
     }
 }
