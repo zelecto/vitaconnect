304b67ae079d0f6cdd4145f19346a771fed3f3b7
zelecto <ca346364@gmail.com>
2024-09-13 15:11:15 -0500

    creacion de visata imagen del perfil

---
+++ resources/views/components/image-perfil.blade.php
@@ -0,0 +1,4 @@
+<div class="flex items-center gap-4">
+    <img src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/e40b6ea6361a1abe28f32e7910f63b66/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"
+        {{ $attributes->merge(['class' => 'size-32 rounded-full object-cover']) }} />
+</div>
