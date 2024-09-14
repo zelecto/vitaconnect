6226a615637be85b5b68265a4ad62ce33e91ee5c
zelecto <ca346364@gmail.com>
2024-09-14 08:58:40 -0500

    modificacion y agregacion de nuevos metodos en las rutas

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PublicationsController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', LoginController::class);
+
 Route::post('/Autenticar', [LoginController::class, 'autenticar']);
 
 Route::post('/RegisterUser', [UserController::class, 'store']);
 
-Route::get('/Home/{user}', HomeController::class);
+Route::get('/Home/{email}', HomeController::class);
+
+Route::post('/CreatePost', [PublicationsController::class, 'create'])->name('create.post');
